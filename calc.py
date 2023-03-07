0d4c0c08a701a7d67bf309fc21e6112cf40f4bce
charming-ak <126399322+charming-ak@users.noreply.github.com>
2023-03-07 16:32:10 +0530

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,6 @@
+add=lambda a,b:a+b
+multi=lambda a,b:a*b
+div=lambda a,b:a/b
+
+result=div(118,4)
+print(result)
---
+++ forloop.py
@@ -0,0 +1,47 @@
+#for x in range(6):
+#    print(x)
+
+#for x in range(6):
+#    print(x+1)
+
+#for x in range(1,6):S
+#    print(x)
+
+#for x in range(1,101,5):
+#    print(x)
+
+#persons = {'krishna','anil','malli','vani','amma','bharathi'}
+#for user in persons:
+#    print(user)
+
+#name = "Dommala Anil Kumar"
+#for zandubalm in name:
+#    print(zandubalm)
+#    if zandubalm == "u":
+#        break
+
+#familymembers = ['krishna','anil','vani','amma','bharathi','mallikarjuna']
+#for total in familymembers:
+#    print(total)
+#    if total == 'bharathi':
+#        break
+
+#familymembers = ['krishna','anil','vani','amma','bharathi','mallikarjuna']
+#for total in familymembers:
+#    if total == 'mallikarjuna':
+#        break
+#    print(total)
+
+#familymembers = ['krishna','anil','mallikarjuna','vani','amma','bharathi']
+#for total in familymembers:
+#    if total == 'mallikarjuna':
+#        continue
+#    print(total)
+
+familymembers = ['krishna','anil','mallikarjuna','vani','amma','bharathi']
+for x in familymembers:
+    print(x)
+    if x=='mallikarjuna':
+        break
+else:
+    print("bye")
